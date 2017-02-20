7c3c1a0aefb63a47ac7a7504b4c55b1733df2503
Dan Segal <danSegal2@gmail.com>
2017-02-20 16:55:25 -0500

    Add .keep files to folders in init function

--- src/functions/init.jl
+++ src/functions/init.jl
@@ -14,7 +14,10 @@ function init()
 
   for folder in folders
     for parent_folder in parent_folders
-      mkdir("$(pwd())/$parent_folder/$folder")
+      folder_path = "$(pwd())/$parent_folder/$folder"
+
+      mkdir(folder_path)
+      touch("$folder_path/.keep")
     end
   end
 
