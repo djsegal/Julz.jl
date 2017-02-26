9e6c401dc455f50cda17280ede986937576f9e99
Dan Segal <danSegal2@gmail.com>
2017-02-25 21:49:40 -0500

    Have ‘julz new’ generate project in same directory

--- src/functions/new.jl
+++ src/functions/new.jl
@@ -2,10 +2,11 @@ function new(app_name, license)
 
   cleaned_app_name = get_class_name(app_name)
 
-  PkgDev.generate(cleaned_app_name, license)
-
   initial_dir = pwd()
-  package_dir = "$(Pkg.dir())/$cleaned_app_name"
+
+  PkgDev.generate(cleaned_app_name, license, path=initial_dir)
+
+  package_dir = "$initial_dir/$cleaned_app_name"
 
   cd(package_dir)
   init()
