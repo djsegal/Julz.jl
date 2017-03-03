adc717319955301589c6a2cd14e45963f6085b35
Dan Segal <danSegal2@gmail.com>
2017-03-03 01:41:01 -0500

    Remove default test so runspec works off the bat

--- src/functions/init.jl
+++ src/functions/init.jl
@@ -12,6 +12,8 @@ function init()
     "types"
   ]
 
+  remove_default_test()
+
   for parent_folder in parent_folders
     add_code_to_main_file(parent_folder)
 
@@ -24,6 +26,20 @@ function init()
     end
   end
 
+end
+
+function remove_default_test()
+  test_file_path = "$(pwd())/test/runtests.jl"
+  test_file_text = readstring(test_file_path)
+
+  line_1 = "# write your own tests here"
+  line_2 = "@test 1 == 2"
+  default_test = "\n$line_1\n$line_2\n"
+
+  new_file = replace(test_file_text, default_test, "")
+  open(test_file_path, "w") do old_file
+    write(old_file, new_file)
+  end
 end
 
 function add_code_to_main_file(parent_folder)
