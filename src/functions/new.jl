36f9bf8605a6eba094412d9ef06aaa2ef04405db
Dan Segal <danSegal2@gmail.com>
2017-03-03 01:26:09 -0500

    Make new command more resilient against “.jl” use

--- src/functions/new.jl
+++ src/functions/new.jl
@@ -1,8 +1,9 @@
 function new(pkg_name, license)
 
-  cleaned_pkg_name = get_class_name(pkg_name)
-
   initial_dir = pwd()
+
+  cleaned_pkg_name = get_class_name(pkg_name)
+  cleaned_pkg_name = replace(cleaned_pkg_name, ".jl", "")
 
   PkgDev.generate(cleaned_pkg_name, license, path=initial_dir)
 
@@ -11,5 +12,7 @@ function new(pkg_name, license)
   cd(package_dir)
   init()
   cd(initial_dir)
+
+  mv(package_dir, "$package_dir.jl")
 
 end
