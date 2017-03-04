578c5b30b5215f611bc45358a1de5a9fb5e182a9
Dan Segal <danSegal2@gmail.com>
2017-03-04 15:14:16 -0500

    Allow Julz test suites to be run with Pkg.test(..)

--- templates/test/runtests.jl
+++ templates/test/runtests.jl
@@ -1,5 +1,6 @@
 
 @testset "All Tests" begin
+  if ( endswith(pwd(), "/test") ) ; cd("..") ; end
   Julz.include_all_files("$(pwd())/test")
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Julz
 using Base.Test
 
 @testset "All Tests" begin
+  if ( endswith(pwd(), "/test") ) ; cd("..") ; end
   Julz.include_all_files("$(pwd())/test")
 end
 
