ba415c23e34784a1b1d78ba3ad0a3c79ffa1a95e
Dan Segal <danSegal2@gmail.com>
2017-03-04 17:27:19 -0500

    Add test for user_input

--- test/functions/user_input_test.jl
+++ test/functions/user_input_test.jl
@@ -2,4 +2,29 @@
 
   @test isdefined(Julz, :user_input) == true
 
+  filename = tempname()
+  open(filename, "w") do f
+    println(f, "yes")
+  end
+
+  originalSTDOUT = STDOUT
+
+  (outRead, outWrite) = redirect_stdout()
+
+  answer = open(filename) do f
+    redirect_stdin(f) do
+      Julz.user_input("Yes or no? ")
+    end
+  end
+
+  close(outWrite)
+
+  close(outRead)
+
+  redirect_stdout(originalSTDOUT)
+
+  rm(filename)
+  @test contains(answer, "yes")
+  @test !contains(answer, "no")
+
 end
