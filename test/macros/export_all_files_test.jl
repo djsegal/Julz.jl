bda0b1ddd1fc31e8988fa12f81e2ea76eb4094ed
Dan Segal <danSegal2@gmail.com>
2017-03-05 02:42:28 -0500

    Add test for “export_all_files” macro

--- test/macros/export_all_files_test.jl
+++ test/macros/export_all_files_test.jl
@@ -1,5 +1,23 @@
+initial_dir = pwd()
+
+cd("dummy")
+
+Julz.generate("function", "roo")
+
+module Woof
+  using Julz
+  Julz.include_all_files("$(pwd())/src/functions")
+  Julz.@export_all_files "$(pwd())/src/functions"
+end
+
 @testset "Export All Files Macro Tests" begin
 
   @test isdefined(Julz, Symbol("@export_all_files")) == true
+
+  @test isdefined(Woof, :roo) == true
+
+  Julz.destroy("function", "roo")
+
+  cd(initial_dir)
 
 end
