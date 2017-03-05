eef2aeb6862f11e6b5b1b28c281917c66e1d0977
Dan Segal <danSegal2@gmail.com>
2017-03-04 21:31:14 -0500

    Add test for “get_all_files” function

--- test/functions/get_all_files_test.jl
+++ test/functions/get_all_files_test.jl
@@ -2,4 +2,24 @@
 
   @test isdefined(Julz, :get_all_files) == true
 
+  initial_dir = pwd()
+
+  cd("dummy")
+
+  @test length(Julz.get_all_files("../dummy")) == 1
+
+  Julz.generate("type", "foo")
+
+  @test length(Julz.get_all_files("../dummy")) == 3
+
+  Julz.destroy("type", "foo")
+
+  @test length(Julz.get_all_files("../dummy")) == 1
+
+  cd(initial_dir)
+
+  main_file = Julz.get_all_files("dummy")[1]
+
+  @test main_file == "dummy/src/JulzDummy.jl"
+
 end
