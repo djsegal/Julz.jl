f17cfd0a98b6c9970efbf12645f9d83a0d940c3f
Dan Segal <danSegal2@gmail.com>
2017-03-04 19:02:31 -0500

    Add tests for “title” and “get_class_name” methods

--- test/methods/get_class_name_test.jl
+++ test/methods/get_class_name_test.jl
@@ -2,4 +2,14 @@
 
   @test isdefined(Julz, :get_class_name) == true
 
+  @test_throws MethodError Julz.get_class_name(1)
+
+  @test Julz.get_class_name("first") == "First"
+
+  @test Julz.get_class_name("first class") == "FirstClass"
+
+  @test Julz.get_class_name("first_class") == "FirstClass"
+
+  @test Julz.get_class_name("first-class") == "First-class"
+
 end
--- test/methods/title_test.jl
+++ test/methods/title_test.jl
@@ -2,4 +2,14 @@
 
   @test isdefined(Julz, :title) == true
 
+  @test_throws MethodError Julz.title(1)
+
+  @test Julz.title("first") == "First"
+
+  @test Julz.title("first class") == "First Class"
+
+  @test Julz.title("first_class") == "First Class"
+
+  @test Julz.title("first-class") == "First-class"
+
 end
