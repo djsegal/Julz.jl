d0290596c961dbb849c9ded9dae1cc6d18f1e5f3
Dan Segal <danSegal2@gmail.com>
2017-03-05 01:59:01 -0500

    Add test for “include_all_files” function

--- test/functions/include_all_files_test.jl
+++ test/functions/include_all_files_test.jl
@@ -2,4 +2,20 @@
 
   @test isdefined(Julz, :include_all_files) == true
 
+  initial_dir = pwd()
+
+  cd("dummy")
+
+  Julz.generate("function", "baz")
+
+  @test isdefined(:baz) != true
+
+  Julz.include_all_files("$(pwd())/src/functions")
+
+  @test isdefined(:baz) == true
+
+  Julz.destroy("function", "baz")
+
+  cd(initial_dir)
+
 end
