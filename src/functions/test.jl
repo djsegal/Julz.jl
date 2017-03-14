2bd802547c3e14a361fa95b1c372fed1afc317c9
Dan Segal <danSegal2@gmail.com>
2017-03-14 18:04:38 -0400

    Make test function work w/o linking locally

--- src/functions/test.jl
+++ src/functions/test.jl
@@ -1,4 +1,21 @@
 function test()
+
   package_name = rsplit(pwd(), "/"; limit=2)[2]
-  Pkg.test(package_name)
+
+  package_dir = rsplit(dirname(@__FILE__), "/"; limit=4)[1]
+
+  target_name = replace(package_name, ".jl", "")
+
+  target_path = "$package_dir/$target_name"
+
+  if target_path != pwd()
+    initial_dir = pwd()
+
+    cd("..")
+    cp(package_name, target_path, remove_destination=true)
+    cd(initial_dir)
+  end
+
+  Pkg.test(target_name)
+
 end
