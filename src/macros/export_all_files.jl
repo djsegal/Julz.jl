6e073d5a27fd5b159e2c1e659f2358e018f7707b
djsegal <dansegal2@gmail.com>
2017-11-16 03:03:02 -0500

    Fix bug where files weren’t actually exported

--- src/macros/export_all_files.jl
+++ src/macros/export_all_files.jl
@@ -3,11 +3,20 @@ macro export_all_files(cur_item)
 
   cur_expression = quote end
   for file in all_files
-    exported_object = Symbol("$(file)")
+    cur_export = split(file, "/")[end]
+
+    !endswith(cur_export, ".jl") && continue
+
+    cur_export = replace(cur_export, ".jl", "")
+
+    exported_object = Symbol(cur_export)
+
     cur_block = quote
       export $(esc(exported_object))
     end
+
     append!(cur_expression.args, cur_block.args)
   end
+
   cur_expression
 end
